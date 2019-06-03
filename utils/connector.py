26e8354892b3ed725c0940d5541dcc324957df80
unknown <VCStudent@4201-6T519N2.uottawa.o.univ>
2019-06-03 13:51:33 -0400

    NCBI connector and blast functions

--- utils/connector.py
+++ utils/connector.py
@@ -1,5 +1,42 @@
 """ utils/connector.py Connects to Genbank DB """
 
+from Bio import Entrez
+from Bio.Blast import NCBIWWW
+from Bio.Blast import NCBIXML
 
+from dataclasses import dataclass
+from typing import List
+
+import urllib
+
+
+@dataclass
 class BioConnect:
     """ Provides methods to download and run blast on the NCBI servers. """
+
+    expect: int
+    hit: int
+    db = 'nucleotide'
+    fileFormat = 'XML'
+    flavour = 'tblastn'
+    service = 'psi'
+    blastdb = 'nr'
+
+    def load(self, accession):
+        """ Download Genbank record from NCBI-Genbank. """
+        try:
+            handle = Entrez.efetch(self.db, accession, "gbwithparts", 'txt')
+            return handle
+        except urllib.error.HTTPError as error:
+            print(error.read())
+
+    def bioBlast(self, seq) -> List:
+        handle = NCBIWWW.qblast(self.flavour, self.blastdb, seq, self.fileFormat, self.hit, self.expect, self.service)  # noqa
+        record = NCBIXML.parse(handle)
+        numList = list()
+
+        for rec in record:
+            for align in rec.alignments:
+                numList.append(align.accession)
+
+        return numList
